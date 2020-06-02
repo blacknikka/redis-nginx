13de1f536a707061818a1eb8273b78244632f4b5
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-06-02 23:11:30 +0900

    add redis session

--- app/src/main.go
+++ app/src/main.go
@@ -2,55 +2,58 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"net/http"
 	"strconv"
 
-	"github.com/go-redis/redis"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
-		DB:       0,
-	})
+func Index(c *gin.Context) {
+	session := sessions.Default(c)
+	var mysession int
+	if v := session.Get("mysession"); v != nil {
+		switch v.(type) {
+		case string:
+			mysession, _ = strconv.Atoi(v.(string))
+			mysession++
+			session.Set("mysession", strconv.Itoa(mysession))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		value, err := getData(client, "mykey")
-		var counter int
-		if err != nil {
-			putData(client, "mykey", strconv.Itoa(1))
-			counter = 1
-		} else {
-			counter, _ = strconv.Atoi(value)
-			counter++
-			putData(client, "mykey", strconv.Itoa(counter))
+			if err := session.Save(); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+				return
+			}
+		default:
+			session.Delete("mysession")
+			session.Set("mysession", strconv.Itoa(1))
+			mysession = 1
+			if err := session.Save(); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+				return
+			}
 		}
-		fmt.Fprintf(w, "counter = %d\n", counter)
-		fmt.Fprintf(w, "Hello this is server 1, %q", html.EscapeString(r.URL.Path))
-	})
+	} else {
+		fmt.Println("no session key.")
+		session.Set("mysession", strconv.Itoa(1))
 
-	http.ListenAndServe(":8080", nil)
-}
-
-func putData(client *redis.Client, key string, value string) error {
-	// Set
-	err := client.Set(key, value, 0).Err()
-	if err != nil {
-		fmt.Println("redis.Client.Set Error:", err)
-		return err
-	}
-	return nil
-}
-
-func getData(client *redis.Client, key string) (string, error) {
-	// Set
-	val, err := client.Get(key).Result()
-	if err != nil {
-		fmt.Println("redis.Client.Get Error:", err)
-		return "", err
+		if err := session.Save(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+			return
+		}
+		mysession = 1
 	}
 
-	return val, nil
+	str := fmt.Sprintf("this is server 1, counter = %d\n", mysession)
+
+	c.JSON(http.StatusOK, str)
+}
+
+func main() {
+	r := gin.Default()
+	store, _ := redis.NewStore(10, "tcp", "redis:6379", "", []byte("secret"))
+	r.Use(sessions.Sessions("session", store))
+	r.GET("/", Index)
+
+	r.Run(":8080")
 }
